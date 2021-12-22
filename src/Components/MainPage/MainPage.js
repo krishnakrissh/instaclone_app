a05ed39d0335e13169a22626a013fe9552be05ed
krishna krissh <krishna7krissh@gmail.com>
2021-12-22 09:12:57 +0530

    Main page and post UI

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,18 +1,54 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './MainPage.css';
 import Post from '../Post/Post';
 
 class MainPage extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            postArray: []
+        }
     }
+
+    componentDidMount() {
+        this.getPost();
+    }
+
+    getPost() {
+        let data = [
+            {
+                "postId": "1",
+                "userName": "Krishna",
+                "postImage": "https://wallpaperaccess.com/full/323408.jpg",
+                "timeStamp": "675867576",
+                "likes": "763"
+            },
+            {
+                "postId": "1",
+                "userName": "seku",
+                "postImage": "https://wallpaperaccess.com/full/408982.jpg",
+                "timeStamp": "675867576",
+                "likes": "763"
+            },
+            {
+                "postId": "1",
+                "userName": "aadu",
+                "postImage": "https://cdn.vox-cdn.com/thumbor/2n-5muv52m1dN_fDVWot_YM7AOk=/0x26:640x453/1400x1400/filters:focal(0x26:640x453):format(jpeg)/cdn.vox-cdn.com/assets/3715801/amazing_spider_man_2.jpg",
+                "timeStamp": "675867576",
+                "likes": "763"
+            }
+        ];
+        this.setState({ postArray: data });
+    }
+
     render() {
         return (
             <div>
-                <Post />
-                <Post />
-                <Post />
+                {
+                    this.state.postArray.map((item, index) => (
+                        <Post id={item.postId} userName={item.userName} postImage={item.postImage} likes={item.likes} />
+                    ))
+                }
             </div>
         );
     }
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './Post.css';
-import { Avatar } from '@material-ui/core';
-import postimage from '../../images/post.jpg'
+import { Avatar, ListItem } from '@material-ui/core';
 import love from '../../images/love.svg'
 import comment from '../../images/comment.svg'
 import share from '../../images/share.svg'
@@ -9,21 +8,49 @@ import share from '../../images/share.svg'
 class Post extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            commentList: []
+        }
     }
+    componentDidMount() {
+        this.getComments();
+    }
+    getComments = () => {
+        let data = [
+            {
+                "username": "Krissh",
+                "commentId": "1",
+                "timestamp": "121542156",
+                "description": "Comment 1"
+            },
+            {
+                "username": "Aadu",
+                "commentId": "1",
+                "timestamp": "121542156",
+                "description": "Comment 2"
+            },
+            {
+                "username": "seku",
+                "commentId": "1",
+                "timestamp": "121542156",
+                "description": "Comment 3"
+            }
+        ];
+        this.setState({ commentList: data });
+    }
+
     render() {
         return (
             <div className='post_container'>
                 <div className='post_header'>
-                    <Avatar className='post_image' src='' />
-                    <div className='post_username'> username </div>
+                    <Avatar className='post_image' src={this.props.profileImage} />
+                    <div className='post_username'>{this.props.userName}</div>
                 </div>
 
                 {/* Images */}
                 <div>
-                    <img src={postimage} width="615px"></img>
+                    <img src={this.props.postImage} width="615px"></img>
                 </div>
-
 
                 <div>
                     <div style={{ "marginLeft": "10px" }}>
@@ -31,17 +58,20 @@ class Post extends Component {
                         <img src={comment} className="post_reactimage" />
                         <img src={share} className="post_reactimage" />
                     </div>
-                    <div style={{"fontWeight":"bold","marginLeft":"15px"}}>
-                        548 likes
+                    <div style={{ "fontWeight": "bold", "marginLeft": "15px" }}>
+                        {this.props.likes} likes
                     </div>
                 </div>
 
                 {/* Comment */}
                 <div>
-                    <div className="post_comment">super macha</div>
-                    <div>super macha</div>
-                    <div className="post_comment">super macha</div>
-                    <div>super macha</div>
+                    <div>{
+                        this.state.commentList.map((item, index)=>(
+                        <div classaName="post_comment">{item.username}:{item.description}</div>
+                        ))
+                    }
+                    </div>
+
                     <input type="text" className="post_addcomment" placeholder='Add a comment...' />
                 </div>
             </div>
